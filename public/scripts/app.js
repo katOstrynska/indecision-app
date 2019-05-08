2fbf5781fef1088429ff52945f16f646899424cb
unknown <k.ostrynska@gmail.com>
2019-05-08 13:13:51 +0200

    Counter, arrays

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,95 @@
 'use strict';
 
-//arguments object - no longer bound with arrow functions
+console.log('app.js is running');
 
-var add = function add(a, b) {
-    console.log(arguments);
-    return a + b;
+//JSX
+var app = {
+    title: 'Titleeee',
+    subtitle: 'Subtitleee',
+    options: []
 };
 
-var addArrow = function addArrow(a, b) {
-    // console.log(arguments);  //we dont's have access to arguments
-    return a + b;
-};
-console.log(addArrow(23, 23, 89));
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
 
-//this keyword - no longer bound to arrow functions
+    var option = e.target.elements.option.value;
 
-var user = {
-    name: 'Bob',
-    cities: ['Mexico', 'Moscow', 'Dublin'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
-        // this.cities.forEach((city) => {
-        //     console.log(this.name + ' has lived in ' + city);
-        // });
-
-        // const that = this;
-        // this.cities.forEach(function(city) {
-        //     console.log(this.name + ' has lived in ' + city);
-        // });
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
     }
 };
 
-console.log(user.printPlacesLived());
-
-var multiplier = {
-    numbers: [1, 6],
-    multiplyBy: 2,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this2.multiplyBy;
-        });
-    }
+var onRemoveAll = function onRemoveAll() {
+    app.options = [];
+    render();
 };
 
-console.log(multiplier.multiply());
+var appRoot = document.getElementById('app');
+
+var numbers = [55, 101, 1000];
+
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'button',
+            { onClick: onRemoveAll },
+            'Remove All'
+        ),
+        numbers.map(function (number) {
+            return React.createElement(
+                'p',
+                { key: number },
+                'Number : ',
+                number
+            );
+        }),
+        React.createElement(
+            'ol',
+            null,
+            app.options.map(function (option) {
+                return React.createElement(
+                    'li',
+                    { key: option },
+                    option
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add option'
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+render();
--- src/app.js
+++ src/app.js
@@ -1,44 +1,64 @@
 console.log('app.js is running');
 
 //JSX
-const titleObj = {
+const app = {
     title: 'Titleeee',
     subtitle: 'Subtitleee',
-    options: ['one', 'two']
-};
-const template = (
-    <div>
-        <h1>{titleObj.title}</h1>
-        {titleObj.subtitle && <p>{titleObj.subtitle}</p>}
-        <p>{titleObj.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-            <li>item one</li>
-            <li>item two</li>
-        </ol>
-    </div>
-);
-const user = {
-    name: 'Bob',
-    age: 18,
-    location: 'Texas'
+    options: []
 };
 
-function getLocation(location) {
-    if (location) {
-        return <p>Location: {location}</p>;
+const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
     }
+};
+
+const onRemoveAll = () => {
+    app.options = [];
+    render();
 }
 
-const templateTwo = (
-    <div>
-        <h1>{user.name ? user.name : 'Anonymous'}</h1>
-        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
-    </div>
-);
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(
-    template,
-    appRoot
-);
+const numbers = [55, 101, 1000];
+
+const render = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <button onClick={onRemoveAll}>Remove All</button>
+            {
+                numbers.map((number) => {
+                    return <p key={number}>Number : {number}</p>;
+                })
+            }
+            <ol>
+                {
+                    app.options.map((option) => {
+                        return <li key={option}>{option}</li>
+                    })
+                }
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
+                <button>Add option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(
+        template,
+        appRoot
+    );
+}
+
+render();
+
