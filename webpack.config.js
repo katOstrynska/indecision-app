2b1648bb28337e4176736bdfcb3035e09fe5a193
unknown <k.ostrynska@gmail.com>
2019-05-13 11:23:47 +0200

    ES^ import/export

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// entry -> output
+//entry - where webpack should start (for us it's app.js in src folder)
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
+};
