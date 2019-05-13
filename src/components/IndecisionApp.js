e141a2dba77664feae2b1cef6346d3554b71c1e7
unknown <k.ostrynska@gmail.com>
2019-05-13 16:54:32 +0200

    Webpack

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -4,17 +4,45 @@ import AddOption from './AddOption';
 import Action from './Action';
 import Header from './Header';
 import Options from './Options';
+import OptionModal from './OptionModal'
 
 export default class IndecisionApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.state = {
+    state = {
+        options: [],
+        selectedOption: undefined
+    };
+    handleDeleteOptions = () => {
+        this.setState(() => ({
             options: []
+        }));
+    }
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option) => optionToRemove !== option)
+        }));
+    }
+    handlePick = () => {
+        const random = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[random];
+        this.setState (() => ({
+            selectedOption: option
+        }));
+    }
+    handleAddOption = (option) => {
+        if (!option) {
+            return 'Enter valid value to add item';
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists';
         }
+        
+        this.setState((prevState) => ({
+            options: prevState.options.concat(option)
+        }));
+    }
+    handleClearSelectedOption = () => {
+        this.setState (() => ({
+            selectedOption: undefined
+        }));
     }
     componentDidMount() {
         try {
@@ -41,32 +69,6 @@ export default class IndecisionApp extends React.Component {
     componentWillUnmout() {
         console.log('component will unmount');
     }
-    handleDeleteOptions() {
-        this.setState(() => ({
-            options: []
-        }));
-    }
-    handleDeleteOption(optionToRemove) {
-        this.setState((prevState) => ({
-            options: prevState.options.filter((option) => optionToRemove !== option)
-        }));
-    }
-    handlePick() {
-        const random = Math.floor(Math.random() * this.state.options.length);
-        const option = this.state.options[random];
-        alert(option);
-    }
-    handleAddOption(option) {
-        if (!option) {
-            return 'Enter valid value to add item';
-        } else if (this.state.options.indexOf(option) > -1) {
-            return 'This option already exists';
-        }
-        
-        this.setState((prevState) => ({
-            options: prevState.options.concat(option)
-        }));
-    }
     render() {
         const subtitle = 'Put your life in hands of a computer';
         return (
@@ -83,6 +85,10 @@ export default class IndecisionApp extends React.Component {
                 />
                 <AddOption
                     handleAddOption={this.handleAddOption}
+                />
+                <OptionModal
+                    selectedOption={this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}
                 />
             </div>
         )
--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,22 @@ module.exports = {
     },
     module: {
         rules: [{
-            loader: 'babel-loader',
             test: /\.js$/,
-            exclude: /node_modules/
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        '@babel/preset-env'
+                    ]
+                }
+            }
         }]
-    }
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 8080
+    },
+    mode: 'development'
 };
