4487d73d049ca2b11d963b5db63319d03be04400
unknown <k.ostrynska@gmail.com>
2019-05-14 12:09:05 +0200

    Styling

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -20,11 +20,14 @@ export default class AddOption extends React.Component {
         return (
             <div>
                 {
-                    this.state.error && <p>{this.state.error}</p>
+                    this.state.error && <p className="add-option-error">{this.state.error}</p>
                 }
-                <form onSubmit={this.handleAddOption}>
-                    <input type="text" name="option" />
-                    <button>Add option</button>
+                <form
+                    className="add-option"
+                    onSubmit={this.handleAddOption}
+                >
+                    <input className="add-option__input" type="text" name="option" />
+                    <button className="button">Add option</button>
                 </form>
             </div>
         )
